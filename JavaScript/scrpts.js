6eee9ee6afc641e158290830325219df57e2c2ef
dholland <dholl018@odu.edu>
2021-10-05 23:18:26 -0400

    Fixed password validation

--- JavaScript/scrpts.js
+++ JavaScript/scrpts.js
@@ -1,6 +1,6 @@
 $(document).ready(function (){
 
-
+    changeTextBoxColor();
     $('#password-signup').on('input',function (){
         passwordValidation()
     })
@@ -8,13 +8,22 @@ $(document).ready(function (){
         passwordValidation()
     })
     searchQuery();
-
-    $("<h3>TEST</h3>").appendTo("#section");
+    $("input.textbox[type=text]").on('input',function (){
+        changeTextBoxColor();
+    })
 
 
 
 })
 
+function changeTextBoxColor(){
+    if($("input.textbox[type=text]").val() === ''){
+        $("input.textbox[type=text]").addClass("requiredBorder");
+    }
+    else{
+        $("input.textbox[type=text]").removeClass("requiredBorder");
+    }
+}
     ///Query String
 function searchQuery(){
     var queryURL = window.location.search;
@@ -43,10 +52,11 @@ function searchQuery(){
                 $('.Report-content').append("<p>1.  <a href='./PrivacyPolicy.html'>Privacy Policy</a></p>");
                 break;
             case 'REPORT':
+            case 'REPROT'://common misspelling
                 $('#search-header').text('Search results found for the term ' + "\""+keyword+"\"");
-                $('.Report-content').append("<p>1.  <a href='./PrivacyPolicy.html'>Privacy Policy</a></p>");
-                $('.Report-content').append("<p>2.  <a href='./PrivacyPolicy.html'>Privacy Policy</a></p>");
-                $('.Report-content').append("<p>3.  <a href='./PrivacyPolicy.html'>Privacy Policy</a></p>");
+                $('.Report-content').append("<p>1.  <a href='./add_Item'>New Safety Report</a></p>");
+                $('.Report-content').append("<p>2.  <a href='./MySafetyReports.html'>My Reports</a></p>");
+                $('.Report-content').append("<p>3.  <a href='./List.html'>All Reports</a></p>");
                 break;
             case 'LAB':
                 $('#search-header').text('Search results found for the term ' + "\""+keyword+"\"");
@@ -67,19 +77,40 @@ function searchQuery(){
 
     ///Validates Password
     function passwordValidation(){
-        let password = $('#password-signup').val()
-        let retypePassword = $('#retype-pass').val()
+        let password = $("#password-signup").val();
+        let retypePassword = $('#retype-pass').val();
         if (password.length >= 6) {
-            $('#Six-Character').text('Password is at least 6 characters').css("color","green")
+            $('#Six-Character').text('Password is at least 6 characters').css("color","green");
         }
         else{
-            $('#Six-Character').text('Password MUST be at 6 characters').css("color","red")
+            $('#Six-Character').text('Password MUST be at 6 characters').css("color","red");
+        }
+        //Test if password matches retype
+        if(password === retypePassword){
+            $('#matching-Password').text('Password match').css("color","green");
+        }
+        else {
+            $('#matching-Password').text('Password DO NOT match').css("color","red");
+        }
+        ///contains upper case
+        if(password.match(/[A-Z]/)){
+            $('#contains-upper').text('Password contains at least 1 upper case character').css("color","green");
+        }
+        else{
+            $('#contains-upper').text('Password DOES NOT contain at least 1 upper case').css("color","red");
         }
 
-        if(password === retypePassword){
-            $('#matching-Password').text('Password match').css("color","green")
+        if(password.match(/[a-z]/)){
+            $('#contains-lower').text('Password contains at least 1 lower case character').css("color","green");
         }
         else{
-            $('#matching-Password').text('Password DO NOT match').css("color","red")
+            $('#contains-lower').text('Password DOES NOT contain at least 1 lower case').css("color","red");
+        }
+        ///Contains number
+        if(password.match(/[0-9]/)){
+            $('#contains-number').text('Password contains at least 1 number').css("color","green");
+        }
+        else{
+            $('#contains-number').text('Password DOES NOT contain at least 1 number').css("color","red");
         }
     }
