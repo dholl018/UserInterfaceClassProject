8126c0a108f963896fe0de0196c79df5f11d61b0
dholland <dholl018@odu.edu>
2021-11-19 14:21:46 -0500

    removed "_db" to sessions

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -148,4 +148,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached'
